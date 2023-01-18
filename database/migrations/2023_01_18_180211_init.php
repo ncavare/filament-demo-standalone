5aba3acfdbb391fa9ea1a109e06e8943e6080dff
Nicolas Cavaré <ncavare@gmail.com>
2023-01-18 19:10:28 +0100

    db

---
+++ database/migrations/2023_01_18_180211_init.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->date('published_at')->nullable();
+            $table->string('seo_title', 60)->nullable();
+            $table->string('seo_description', 160)->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('posts_id')->nullable();
+            $table->morphs('commentable');
+            $table->text('title')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('posts');
+
+        Schema::dropIfExists('comments');
+    }
+};
